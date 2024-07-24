dc8f0fe5d8860708f80e2217c36e1b64638f0250
CassoChan <152133628+CassoChan@users.noreply.github.com>
2024-07-25 00:42:01 +0800

    Add files via upload

--- Env_PC.sh
+++ Env_PC.sh
@@ -1,33 +1,85 @@
 #!/bin/bash
-
-# lib install setting
-ROS_install=true
+############################## lib install setting ##############################
+ROS_install=false
 lib_install=false
-VNC_setting=false
-tty_setting=false
-IP_setting=false
-
-time_setting=false
+vscode_install=false
 ssh_setting=false
 
-PLOT_install=false
-Cartographer_install=false
-FasterLIO_install=false
-KAIST=false
-
-# setting
+##############################       setting       ##############################
+echo "输入电脑型号: 0:RK3588芯片 1:桦汉 2:公司电脑 3:我的私人电脑 4:放弃"
+read Computer_type
+case $Computer_type in
+0)
 user_name="orangepi"
 user_password=orangepi
+Change_Browser=true
+VNC_setting=true
+IP_setting=true
+tty_setting=true
+;;
 
+1)
+user_name="casun"
+user_password=casun36689
+VNC_setting=true
+IP_setting=true
+tty_setting=true
+;;
+
+2)
+user_name="casun"
+user_password=1
+time_setting=true
+IP_setting=true
+;;
+
+3)
+user_name="casso"
+user_password=1
+time_setting=true
+;;
+
+*)
+echo 没有这个选项
+;;
+
+esac
+
+echo "输入需要安装程序: 0:cartographer环境 1:公司3D激光SLAM环境 2:Faster-LIO_SAM 3:Faster-LIO_STD "
+read environment
+case $environment in
+0)
+ceres_install=true
+Cartographer_install=true
+;;
+
+1)
+driver_install=true
+FasterLIO_install=true
+fastlio2localization=true
+;;
+
+2)
+gtsam_install=true
+;;
+
+3)
+gtsam_install=true
+ceres_install=true
+;;
+
+*)
+echo 没有这个选项
+;;
+
+esac
+
+##############################     ROS install    ##############################
 if [ "$ROS_install" = true ];then
 mkdir -p /tmp/fishinstall/tools
 wget http://mirror.fishros.com/install/install.py -O /tmp/fishinstall/install.py 2>>/dev/null 
 source /etc/profile
-# 强制解锁，太多用户遇到这个问题了，没办法，后续想个办法解决下
-# 强解可能会有依赖问题
-# sudo rm /var/cache/apt/archives/lock
-# sudo rm /var/lib/dpkg/lock
-# sudo rm /var/lib/dpkg/lock-frontend
+
 if [ $UID -eq 0 ];then
     apt-get install sudo 
 fi
@@ -38,7 +90,7 @@ sudo rm fishros
 . ~/.bashrc
 fi
 
-# library install
+##############################   library install   ##############################
 if [ "$lib_install" = true ];then
 # lib update
 sudo apt update
@@ -51,25 +103,64 @@ sudo apt install -y vim
 sudo apt install -y ssh
 sudo apt install -y net-tools
 sudo apt install -y htop
-sudo apt install -y pip
+sudo apt install -y cutecom
 sudo apt install -y flameshot
 sudo apt install -y terminator
+sudo apt install -y clang-format
+
+sudo apt install -y pip
 sudo apt install -y libpcap-dev
 sudo apt install -y pcl-tools
 sudo apt install -y ros-noetic-serial
 sudo apt install -y libyaml-cpp-dev
-sudo apt install -y libgoogle-glog-dev 
+sudo apt install -y libeigen3-dev
+sudo apt install -y libgoogle-glog-dev
+sudo apt install -y libgflags-dev
 
 
-# liosam
+# geographic
+sudo apt-get install -y ros-noetic-geographic-*
+sudo apt-get install -y geographiclib-*
+sudo apt-get install -y libgeographic-*
+sudo ln -s /usr/share/cmake/geographiclib/FindGeographicLib.cmake /usr/share/cmake-3.16/Modules/
+fi
+
+
+###############################      gtsam           ##############################
+if [ "$gtsam_install" = true ];then
 sudo apt-get install -y ros-noetic-navigation
 sudo apt-get install -y ros-noetic-robot-localization
 sudo apt-get install -y ros-noetic-robot-state-publisher
 sudo add-apt-repository ppa:borglab/gtsam-release-4.0
-sudo apt install libgtsam-dev libgtsam-unstable-dev
+sudo apt install -y libgtsam-dev libgtsam-unstable-dev
 fi
 
-# time synchronization
+
+##############################      ceres install     #############################
+if [ "$ceres_install" = true ];then
+sudo apt-get -y install libatlas-base-dev
+sudo apt-get -y install libsuitesparse-dev
+
+echo 输入ceres版本 1: v1.4  2: v2.1
+read ceres_version
+if [ "$ceres_version" = "1" ];then
+git clone -b 1.14.0 https://ceres-solver.googlesource.com/ceres-solver
+elif [ "$ceres_version" = "2" ];then
+git clone -b 2.1.0 https://ceres-solver.googlesource.com/ceres-solver
+else
+echo 输入不符合要求
+fi
+
+cd ceres-solver
+git submodule update --init --recursive
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
+fi
+
+############################## time synchronization ##############################
 if [ "$time_setting" = true ];then
 sudo apt-get install -y ntpdate
 sudo ntpdate time.windows.com
@@ -77,16 +168,17 @@ sudo hwclock --localtime --systohc
 fi
 
 
-# tty setting
+##############################     tty setting     ##############################
 if [ "$tty_setting" = true ];then
 cat > myusb.rules << EOL
 KERNEL=="ttyUSB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE:="0777", SYMLINK+="IMU"
+KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE:="0777", SYMLINK+="MCU"
 EOL
 
 sudo mv myusb.rules /etc/udev/rules.d
 fi
 
-# vnc setting
+##############################     vnc setting     ##############################
 # https://omar2cloud.github.io/rasp/x11vnc/
 if [ "$VNC_setting" = true ];then
 cat > x11vnc.service << EOL
@@ -96,7 +188,7 @@ After=display-manager.service network.target syslog.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd 1
+ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd 123456
 ExecStop=/usr/bin/killall x11vnc
 Restart=on-failure
 
@@ -113,8 +205,10 @@ echo ${user_password} | sudo -S systemctl enable x11vnc.service
 echo ${user_password} | sudo -S systemctl start x11vnc.service
 fi
 
-# IP setting
+##############################      IP setting      ##############################
 if [ "$IP_setting" = true ];then
+
+if [ "$Computer_type" = "0" ];then
 cat > 00-installer-config.yaml << EOL
 network: 
   ethernets: 
@@ -124,12 +218,81 @@ network:
   version: 2
   renderer: networkd
 EOL
+elif [ "$Computer_type" = "1" ];then
+cat > 00-installer-config.yaml << EOL
+network: 
+  ethernets: 
+    enp1s0: 
+      addresses: [192.168.192.11/24]
+      dhcp4: false
+  version: 2
+  renderer: networkd
+EOL
+elif [ "$Computer_type" = "2" ];then
+cat > 00-installer-config.yaml << EOL
+network: 
+  ethernets: 
+    eno1: 
+      addresses: [192.168.192.11/24]
+      dhcp4: false
+  version: 2
+  renderer: networkd
+EOL
+else
+  echo 输入不符合要求
+fi
 
 sudo mv 00-installer-config.yaml /etc/netplan
 sudo netplan apply
 fi
 
-# PLOT install
+
+##############################      Browser Change    ###########################
+if [ "$Change_Browser" = true ];then
+sudo apt install -y iceweasel
+fi
+
+
+##############################    vscode install    ##############################
+if [ "$vscode_install" = true ];then
+sudo apt install -y software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+if [ "$Computer_type" = "1" ] || [ "$Computer_type" = "2" ] || [ "$Computer_type" = "3" ];then
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+elif [ "$Computer_type" = "0" ];then
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+else
+echo There is no such computer type
+exit [0]
+fi
+
+sudo apt update
+sudo apt install -y code
+
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-ceintl.vscode-language-pack-zh-hans
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-iot.vscode-ros
+code --install-extension redhat.vscode-yaml
+code --install-extension eamodio.gitlens
+code --install-extension tomoki1207.pdf
+code --install-extension jeff-hykin.better-cpp-syntax
+code --install-extension xaver.clang-format
+code --install-extension alibaba-cloud.tongyi-lingma
+fi
+
+
+############################## git && ssh setting ##############################
+if [ "$ssh_setting" = true ];then
+git config --global user.name "casun"
+git config --global user.email chenjiacongcjc@163.com
+ssh-keygen -t rsa -C "chenjiacongcjc@163.com"
+ssh -T git@github.com
+fi
+
+
+##############################     PLOT install     ##############################
 if [ "$PLOT_install" = true ];then
 cd
 # plot
@@ -163,7 +326,8 @@ make -j$(nproc)
 sudo make install
 fi
 
-# cartographer install
+
+############################## cartographer install ##############################
 if [ "$Cartographer_install" = true ];then
 cd
 # cartographer lib
@@ -173,8 +337,6 @@ sudo apt-get install -y google-mock
 sudo apt-get install -y libboost-all-dev
 sudo apt-get install -y libcairo2-dev
 sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y libeigen3-dev
-sudo apt-get install -y libgflags-dev
 sudo apt-get install -y libgoogle-glog-dev 
 sudo apt-get install -y liblua5.2-dev 
 sudo apt-get install -y libsuitesparse-dev
@@ -186,15 +348,15 @@ sudo apt -y install python3-sphinx
 sudo apt -y install libgmock-dev
 sudo apt-get -y install libgrpc++
 
-# ceres install
-set -o errexit
-set -o verbose
-cd ~/library/ceres-solver/
-mkdir build
-cd build
-cmake .. -G Ninja -DCXX11=ON
-ninja -j$(nproc)
-sudo ninja install
+# # ceres install
+# set -o errexit
+# set -o verbose
+# cd ~/library/ceres-solver/
+# mkdir build
+# cd build
+# cmake .. -G Ninja -DCXX11=ON
+# ninja -j$(nproc)
+# sudo ninja install
 
 
 # protoc insall
@@ -261,16 +423,8 @@ sudo ninja install
 fi
 
 
-# git && ssh setting
-if [ "$ssh_setting" = true ];then
-git config --global user.name "casun"
-git config --global user.email chenjiacongcjc@163.com
-ssh-keygen -t rsa -C "chenjiacongcjc@163.com"
-ssh -T git@github.com
-fi
-
-# Faster-LIO
-if [ "$FasterLIO_install" = true ];then
+##############################      driver          ##############################
+if [ "$driver_install" = true ];then
 cd
 # livox-SDK
 git clone https://github.com/Livox-SDK/Livox-SDK.git
@@ -289,6 +443,13 @@ git clone https://github.com/Livox-SDK/livox_ros_driver.git
 cd ..
 catkin_make
 
+fi
+
+
+
+##############################      Faster-LIO      ##############################
+if [ "$FasterLIO_install" = true ];then
+
 # Faster-LIO
 cd
 mkdir -p Faster-LIO/src
@@ -297,6 +458,12 @@ git clone https://github.com/gaoxiang12/faster-lio.git
 cd ..
 catkin_make
 
+fi
+
+
+##############################      fast-lio2-map-based-localization      ##############################
+
+if [ "$fastlio2localization" = true ];then
 # sophus
 cd
 git clone https://github.com/strasdat/Sophus.git
@@ -316,35 +483,4 @@ git clone https://github.com/xz00/fast-lio2-map-based-localization.git
 cd ..
 source ~/driver/devel/setup.bash
 catkin_make
-fi
-
-# FAST-LIO-Localization-QN
-if [ "$KAIST" = true ];then
-cd
-sudo apt -y install libomp-dev
-
-# Teaser++
-git clone https://github.com/MIT-SPARK/TEASER-plusplus.git
-cd TEASER-plusplus && mkdir build && cd build
-cmake .. -DENABLE_DIAGNOSTIC_PRINT=OFF -DPYTHON_EXECUTABLE=/usr/bin/python3.8
-sudo make install -j$(nproc)
-sudo ldconfig
-
-# mapping
-mkdir -p ~/mapping/src
-cd ~/mapping/src
-git clone https://github.com/engcang/FAST-LIO-SAM-QN --recursive
-cd ..
-catkin build nano_gicp -DCMAKE_BUILD_TYPE=Release
-catkin build quatro -DCMAKE_BUILD_TYPE=Release -DQUATRO_TBB=ON
-catkin build -DCMAKE_BUILD_TYPE=Release
-
-# Localization
-mkdir -p ~/localization/src
-cd ~/localization/src
-git clone https://github.com/engcang/FAST-LIO-Localization-SC-QN --recursive
-cd ..
-catkin build nano_gicp -DCMAKE_BUILD_TYPE=Release
-catkin build quatro -DCMAKE_BUILD_TYPE=Release -DQUATRO_TBB=ON
-catkin build -DCMAKE_BUILD_TYPE=Release
 fi
